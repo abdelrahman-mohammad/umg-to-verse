76a6b6e6a97a261f30462bde162720ac05728288
Abdelrahman Abdelaal <abdelrahman5abdelaal@gmail.com>
2024-08-23 17:18:23 +0200

    Update

--- src/main/java/io/versetools/umgtoverse/services/KeepAliveService.java
+++ src/main/java/io/versetools/umgtoverse/services/KeepAliveService.java
@@ -1,6 +1,11 @@
 package io.versetools.umgtoverse.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +14,14 @@ import org.springframework.web.client.RestTemplate;
 public class KeepAliveService {
 
     private final RestTemplate restTemplate;
-    private static final String API_URL = "https://umg-to-verse.onrender.com/keep-alive";
+
+    @Value("${API_URL}")
+    private String apiUrl;
+
+    @Value("${API_KEY}")
+    private String apiKey;
+
+    private int count = 0;
 
     @Autowired
     public KeepAliveService(RestTemplate restTemplate) {
@@ -19,8 +31,19 @@ public class KeepAliveService {
     @Scheduled(fixedRate = 600000) // 600,000 milliseconds = 10 minutes
     public void keepAlive() {
         try {
-            String response = restTemplate.getForObject(API_URL, String.class);
-            System.out.println("Keep-alive request sent. Response: " + response);
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("X-API-KEY", apiKey);  // Add the API key as a header
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                    apiUrl,
+                    HttpMethod.GET,
+                    entity,
+                    String.class
+            );
+
+            System.out.println("Keep-alive request sent. Response: " + response.getBody() + " #" + count++);
         } catch (Exception e) {
             System.err.println("Error sending keep-alive request: " + e.getMessage());
         }
