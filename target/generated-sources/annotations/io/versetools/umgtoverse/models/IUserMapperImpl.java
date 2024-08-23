14b8f331cce13173e2e6d82922638dbc0a77af9d
Abdelrahman Abdelaal <abdelrahman5abdelaal@gmail.com>
2024-08-23 17:20:10 +0200

    Update

--- target/generated-sources/annotations/io/versetools/umgtoverse/models/IUserMapperImpl.java
+++ target/generated-sources/annotations/io/versetools/umgtoverse/models/IUserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-23T16:12:00+0200",
+    date = "2024-08-23T17:19:54+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 @Component
